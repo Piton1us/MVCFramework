55347e2d2aa91a902c540f554481620c80f5db26
Piton1us <89169196952@mail.ru>
2023-06-14 20:06:57 +0300

    add folder core

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once 'core/Application.php';
+
+$app = new Application();
+
+
+// $app->router->get('/',function(){
+//    return "hello world";
+// })
+
+
+
+// $app->run();
+
+?>
